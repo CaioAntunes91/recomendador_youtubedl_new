2d2d6c570614b441057f036c985023684f51d076
Caio Antunes <caio.antunes.santos@gmail.com>
2021-07-06 11:07:17 -0300

    first commit data_quality

---
+++ data_quality/DataQuality.py
@@ -0,0 +1,10 @@
+import joblib as jb
+
+class DataQuality(object):
+	def __init__(self):
+		self.title_vec_opt = jb.load("../parameter/title_vec_opt_20210620.pkl.z")
+
+	def apply_tfidfvectorizer(self, df):
+		title_bow_val = self.title_vec_opt.transform(df)
+
+		return title_bow_val
